b5ce9cd0f262932c9db6c1d5f615cbd185b7b464
linzhiqiang <linzhiqiang@shandagames.com>
2020-08-21 13:21:18 +0800

    升级

--- src/KafkaMessageBus/Serializer/MessagePackSerializer.cs
+++ src/KafkaMessageBus/Serializer/MessagePackSerializer.cs
@@ -6,7 +6,11 @@ using System.Text;
 
 namespace Aix.KafkaMessageBus.Serializer
 {
-    public class MessagePackSerializer : ISerializer
+    /// <summary>
+    /// 2.1.152
+    /// </summary>
+   /*
+    public class MessagePackSerializerBak : ISerializer
     {
         private readonly IFormatterResolver _formatterResolver;
         private readonly bool _useCompression;
@@ -33,6 +37,7 @@ namespace Aix.KafkaMessageBus.Serializer
             {
                 return MessagePack.MessagePackSerializer.Deserialize<T>(bytes, _unCompressionOptions);
             }
+
         }
 
         public byte[] Serialize<T>(T data)
@@ -45,6 +50,39 @@ namespace Aix.KafkaMessageBus.Serializer
             {
                 return MessagePack.MessagePackSerializer.Serialize(data, _unCompressionOptions);
             }
+        }
+    }
+
+    */
+
+    /// <summary>
+    /// 1.7.34
+    /// </summary>
+    public class MessagePackSerializer : ISerializer
+    {
+        private readonly IFormatterResolver _formatterResolver;
+        private readonly bool _useCompression;
+
+        public MessagePackSerializer(IFormatterResolver resolver = null, bool useCompression = false)
+        {
+            _useCompression = useCompression;
+            _formatterResolver = resolver ?? ContractlessStandardResolver.Instance;
+        }
+
+        public T Deserialize<T>(byte[] bytes)
+        {
+            if (_useCompression)
+                return MessagePack.LZ4MessagePackSerializer.Deserialize<T>(bytes, _formatterResolver);
+            else
+                return MessagePack.MessagePackSerializer.Deserialize<T>(bytes, _formatterResolver);
+        }
+
+        public byte[] Serialize<T>(T data)
+        {
+            if (_useCompression)
+                return MessagePack.LZ4MessagePackSerializer.Serialize(data, _formatterResolver);
+            else
+                return MessagePack.MessagePackSerializer.Serialize(data, _formatterResolver);
         }
     }
 }
