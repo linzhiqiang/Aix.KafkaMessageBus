cbf0fcfff1075182eb637d131ac0777de4f7496e
linzhiqiang <linzhiqiang@shandagames.com>
2020-12-03 12:55:47 +0800

    升级

--- src/KafkaMessageBus/Serializer/MessagePackSerializerImpl.cs
+++ src/KafkaMessageBus/Serializer/MessagePackSerializerImpl.cs
@@ -58,44 +58,31 @@ namespace Aix.KafkaMessageBus.Serializer
     /// <summary>
     /// 1.7.34
     /// </summary>
-    //public class MessagePackSerializerImpl : ISerializer
-    //{
-    //    private readonly IFormatterResolver _formatterResolver;
-    //    private readonly bool _useCompression;
-
-    //    public MessagePackSerializerImpl(IFormatterResolver resolver = null, bool useCompression = false)
-    //    {
-    //        _useCompression = useCompression;
-    //        _formatterResolver = resolver ?? ContractlessStandardResolver.Instance;
-    //    }
-
-    //    public T Deserialize<T>(byte[] bytes)
-    //    {
-    //        if (_useCompression)
-    //            return MessagePack.LZ4MessagePackSerializer.Deserialize<T>(bytes, _formatterResolver);
-    //        else
-    //            return MessagePack.MessagePackSerializer.Deserialize<T>(bytes, _formatterResolver);
-    //    }
-
-    //    public byte[] Serialize<T>(T data)
-    //    {
-    //        if (_useCompression)
-    //            return MessagePack.LZ4MessagePackSerializer.Serialize(data, _formatterResolver);
-    //        else
-    //            return MessagePack.MessagePackSerializer.Serialize(data, _formatterResolver);
-    //    }
-    //}
-
-    public class MessagePackSerializerDeaultImpl : ISerializer
+    public class MessagePackSerializerImpl : ISerializer
     {
+        private readonly IFormatterResolver _formatterResolver;
+        private readonly bool _useCompression;
+
+        public MessagePackSerializerImpl(IFormatterResolver resolver = null, bool useCompression = false)
+        {
+            _useCompression = useCompression;
+            _formatterResolver = resolver ?? ContractlessStandardResolver.Instance;
+        }
+
         public T Deserialize<T>(byte[] bytes)
         {
-            return MessagePack.MessagePackSerializer.Deserialize<T>(bytes);
+            if (_useCompression)
+                return MessagePack.LZ4MessagePackSerializer.Deserialize<T>(bytes, _formatterResolver);
+            else
+                return MessagePack.MessagePackSerializer.Deserialize<T>(bytes, _formatterResolver);
         }
 
         public byte[] Serialize<T>(T data)
         {
-            return MessagePack.MessagePackSerializer.Serialize(data);
+            if (_useCompression)
+                return MessagePack.LZ4MessagePackSerializer.Serialize(data, _formatterResolver);
+            else
+                return MessagePack.MessagePackSerializer.Serialize(data, _formatterResolver);
         }
     }
 
@@ -103,7 +90,7 @@ namespace Aix.KafkaMessageBus.Serializer
     {
         public static SerializerFactory Instance = new SerializerFactory();
 
-        private static ISerializer Serializer = new MessagePackSerializerDeaultImpl();
+        private static ISerializer Serializer = new MessagePackSerializerImpl();
         private SerializerFactory() { }
 
 
