4047c8a94de0c2446fbf312931c7b5188facc848
linzhiqiang <360779475@qq.com>
2021-05-03 12:11:13 +0800

    twst

--- examples/KafkaMessageBusSample/Model/CmdOptions.cs
+++ examples/KafkaMessageBusSample/Model/CmdOptions.cs
@@ -18,10 +18,10 @@ namespace KafkaMessageBusExample
     /// </summary>
     public class CmdOptions
     {
-        [Option('m', "mode", Required = false, Default = 2, HelpText = "1=生产者测试，2=消费者测试,3=同时测试")]
+        [Option('m', "mode", Required = false, Default = 1, HelpText = "1=生产者测试，2=消费者测试,3=同时测试")]
         public int Mode { get; set; }
 
-        [Option('q', "quantity", Required = false, Default = 5, HelpText = "测试生产数量")]
+        [Option('q', "quantity", Required = false, Default = 1, HelpText = "测试生产数量")]
         public int Count { get; set; }
 
         public static CmdOptions Options;
